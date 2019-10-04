56e09570869bfae75da0bca23bef07029680e430
Christmas <dollerbankas@gmail.com>
2019-10-05 03:03:33 +0330

    navbar have a bug on responsive mode and slider have inapropriate height and jumborton done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
+import { FeaturesComponent } from './features/features.component';
+import { FaqComponent } from './faq/faq.component';
 
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path: '', redirectTo: '/home' , pathMatch: 'full'},
+  {path: 'about', component: AboutComponent},
+  {path: 'contact', component: ContactComponent},
+  {path: 'home' , component: HomeComponent},
+  {path: 'features', component: FeaturesComponent},
+  {path: 'faq', component: FaqComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { FeaturesComponent } from './features/features.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { FaqComponent } from './faq/faq.component';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    FeaturesComponent,
+    AboutComponent,
+    ContactComponent,
+    FaqComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
